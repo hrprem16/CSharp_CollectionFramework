e516d10900aad865a3d57d7dc63b1db7fde43cd4
Prince Bhagat <hrprem16@gmail.com>
2024-02-04 14:47:12 +0530

    [Prince] Add: First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collection_Framework
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
+
+
+
+
