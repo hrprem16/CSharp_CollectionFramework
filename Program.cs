675c9259450e7293ad708947a22efb8f446024c0
Prince Bhagat <hrprem16@gmail.com>
2024-02-06 11:35:49 +0530

    [Prince] Add: Added Dictionary

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Microsoft.VisualBasic;
+using System.Text.RegularExpressions;
 
 namespace Collection_Framework
 {
@@ -11,50 +12,42 @@ namespace Collection_Framework
     {
         public static void Main(string[] args)
         {
-            //C# LinkedList<T>
-            //C# LinkedList<T> class uses the concept of linked list. It allows us to insert and delete elements fastly.
-            //It can have duplicate elements.
+            //C# Dictionary<TKey, TValue> :-
+            //C# Dictionary<TKey, TValue> class uses the concept of hashtable. It stores values on the basis of key.
+            //It contains unique keys only. By the help of key, we can easily search or remove elements
+            //The working of Dictionary is quite similar to the non-generic hashtable.
+            //It is dynamic in nature means the size of the dictionary is grows according to the need
 
-            LinkedList<string> lk1 = new LinkedList<string>(); // Creatin LL of string
-            lk1.AddLast("Sonoo Jaiswal");
-            lk1.AddLast("Ankit");
-            lk1.AddLast("Peter");
-            lk1.AddLast("Irfan");
-            lk1.AddFirst("John");//added to first index  
+            //Example:-
+            Dictionary<string, string> names = new Dictionary<string, string>();
+            names.Add("1", "Sonoo");
+            names.Add("2", "Peter");
+            names.Add("3", "James");
+            names.Add("4", "Ratan");
+            names.Add("5", "Irfan");
 
-            // Iterate list element using foreach loop  
-            foreach (var name in lk1)
+            foreach (KeyValuePair<string, string> kv in names)
             {
-                Console.WriteLine(name);
-            }
-            Console.WriteLine("Element present in LL are: " + lk1.Count);
-
-            //Let's see another example of generic LinkedList<T> class that stores elements before and after specific node.
-            //To get the specific node, we are calling Find() method.
-            LinkedListNode<String> node = lk1.Find("Peter"); //find node which contains peter
-            lk1.AddAfter(node, "Prince"); // Add Node after Peter
-            lk1.AddBefore(node, "Chandru"); // Add
-            Console.WriteLine("After add node at spefic position");
-
-            foreach (var name in lk1)
-            {
-                Console.WriteLine(name);
-            }
-            lk1.Remove("Irfan");
-            lk1.Remove(node);
-            lk1.RemoveFirst();
-            lk1.RemoveLast();
-            Console.WriteLine("*****************");
-            foreach (var name in lk1)
-            {
-                Console.WriteLine(name);
+                Console.WriteLine(kv.Key + " " + kv.Value);
             }
 
-            //Clear(): This method is used to remove all nodes from the LinkedList.
+            Console.WriteLine("Elements in dictionary names : " + names.Count);
 
-            Console.WriteLine("Is Chandru is present in node: " + lk1.Contains("Chandru"));
+            Console.WriteLine("{0}", names.Keys.ElementAt(0));// Find the key at index 0
+            Console.WriteLine(names[names.Keys.ElementAt(0)]); // get he value at index 0\
 
-            
+            //names[key]--> get the value for that key
+            //Clear: This method removes all keys and values from the Dictionary<TKey,TValue>.
+            //Remove: This method removes the value with the specified key from the Dictionary<TKey,TValue>.
+            names.Remove("2");// remove the value and key
+            Console.WriteLine(names.ContainsKey("3")); //This method is used to check whether the Dictionary<TKey,TValue> contains the specified key.
+            Console.WriteLine(names.ContainsValue("James")); //This method is used to check whether the Dictionary<TKey,TValue> contains a specific value.
+
+            Console.WriteLine(names.TryGetValue("4",out string value));
+            string targetValue = "Irfan";
+            string key = names.FirstOrDefault(x => x.Value == targetValue).Key;
+            Console.WriteLine(key);
+
 
         }
     }
